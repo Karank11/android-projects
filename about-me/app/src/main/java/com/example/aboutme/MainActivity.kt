84d4020f251e4d141620b6cee61d6b335063538d
karankarhale <karanp.karhale@gmail.com>
2024-12-26 17:14:12 +0530

    create data class for data binding

--- about-me/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ about-me/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,10 +7,10 @@ import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var myName = MyName("Karan Karhale", "The Batman")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.name.text = "The Batman!!"
+        binding.myName = myName
     }
 }
