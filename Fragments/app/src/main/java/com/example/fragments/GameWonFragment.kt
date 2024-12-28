e47d076d7f3de59a12a872d76c21959e1cbb9f37
karankarhale <karanp.karhale@gmail.com>
2024-12-28 21:57:58 +0530

    navigation safe args

--- Fragments/app/src/main/java/com/example/fragments/GameWonFragment.kt
+++ Fragments/app/src/main/java/com/example/fragments/GameWonFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.example.fragments.databinding.FragmentGameWonBinding
@@ -17,6 +18,9 @@ class GameWonFragment : Fragment() {
     ): View? {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
         binding.nextGameButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_titleFragment))
+
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "correct questions: ${args.numCorrect} && questions attempted: ${args.numQuestions}", Toast.LENGTH_SHORT).show()
         return binding.root
     }
 }
