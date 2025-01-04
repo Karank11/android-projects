663d93b2db92756236a3ea9526ce2644d9316a0c
karankarhale <karanp.karhale@gmail.com>
2025-01-04 20:22:51 +0530

    complete navigation action & pass args between frags

--- app-architecture-ui/app/src/main/java/com/example/wordguesser/screens/title/TitleFragment.kt
+++ app-architecture-ui/app/src/main/java/com/example/wordguesser/screens/title/TitleFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.wordguesser.R
+import com.example.wordguesser.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
+    private lateinit var binding: FragmentTitleBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.gameButton.setOnClickListener {
+            findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+
+        return binding.root
     }
 }
